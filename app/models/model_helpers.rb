c3e27dfd5e33d014b4e3686d6323faf7696018ee
Jamie <jamie@jamie-HP-255-G3-Notebook-PC.(none)>
2016-05-03 21:05:20 +0100

    Changes to some validators and helper files

--- app/models/model_helpers.rb
+++ app/models/model_helpers.rb
@@ -20,15 +20,15 @@ module ModelHelpers
     return date_str self[:start_date]
   end
 
-  def date_str
-    return date_str self[:date]
-  end
-
-  def date_str(date)
+  def date_str(date = self[:date])
     return date.strftime('%d %b %Y')
   end
 
   def amount_str
     return ActionController::Base.helpers.number_to_currency(self[:amount], :unit => "$")
+  end
+
+  def length_in_days
+    return (end_date - self[:start_date]).to_i + 1
   end
 end
--- app/validators/unique_payment_handle_validator.rb
+++ app/validators/unique_payment_handle_validator.rb
@@ -1,10 +1,10 @@
 class UniquePaymentHandleValidator < ActiveModel::EachValidator
-   def validate_each(record, attribute, value)
+  def validate_each(record, attribute, value)
     tenants = Tenant.all
     tenants.each do |t|
       next if t.id == record.id
       if t.payment_handle.include?(record.payment_handle) || record.payment_handle.include?(t.payment_handle)
-        record.errors[:base] << "Submitted payment Handle clashes with existing payment handle (<b><i>#{record.payment_handle}</b></i> & <b><i>#{t.payment_handle}</b></i>)"
+        record.errors[:base] << "Submitted Payment Handle clashes with existing payment handle (<b><i>#{record.payment_handle}</b></i> & <b><i>#{t.payment_handle}</b></i>)"
         break
       end
     end
