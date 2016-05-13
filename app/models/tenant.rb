22b96cf7731d8dc13fc1e93a72470a3d4c06affe
Jamie Guthrie <jamie.guthrie@gmail.com>
2016-05-13 19:21:28 +0100

    Changes to Validation rules for Tenant model
    - Changed some validation rules
    - Changed comments in index view pages

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -6,9 +6,10 @@ class Tenant < ActiveRecord::Base
   validates_associated :house
 
   validates :name, presence: true
-  validates_length_of :name, :payment_handle, :phone_num, minimum: 6, allow_blank: true
+  validates_length_of :name, minimum: 4, allow_blank: true
+  validates_length_of :payment_handle, :phone_num, minimum: 5, allow_blank: true
   validates_uniqueness_of :name
   validates :payment_handle, unique_payment_handle: true
+  validates_format_of :phone_num, :with => /\A\+*[-()0-9\. x]+\z/, allow_blank: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, allow_blank: true
-  validates_format_of :phone_num, :with => /\A\+*[-()0-9\.]+\z/, allow_blank: true
 end
