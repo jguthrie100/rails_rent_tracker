79aa9b5269c95905a18880f48548ed725a5574e6
Jamie Guthrie <jamie.guthrie@gmail.com>
2016-05-16 01:04:37 +0100

    Numerous changes to styling / routing etc
     - Changed styling - extracted styling to application.css
     - Worked on routing when saving/updating tenants/properties. Now you get returned back to the page you came from regardless of whether archiving or restoring a record
     - Archived records are faded out when viewing them on the 'all' records page

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -24,9 +24,9 @@ class PropertiesController < ApplicationController
     property = Property.find(params[:id])
 
     if property.destroy
-      redirect_to properties_path, notice: "Deleted property: <b>'#{property.name}'</b> from the database"
+      redirect_to back_address(""), notice: "Deleted property: <b>'#{property.name}'</b> from the database"
     else
-      redirect_to properties_path, notice: "Failed to delete property: <b>'#{property.name}'</b> from the database: #{property.errors.full_messages.to_sentence}"
+      redirect_to back_address(""), notice: "Failed to delete property: <b>'#{property.name}'</b> from the database: #{property.errors.full_messages.to_sentence}"
     end
   end
 
@@ -36,9 +36,11 @@ class PropertiesController < ApplicationController
     property.archived = true
 
     if property.save
-      redirect_to archived_properties_path, notice: "Archived property: <b>'#{property.name}'</b>."
+      # Using HTTP_REFERER, because we don't know if the page we came from is properties/all or properties/archived etc
+      # split('?')[0] gets rid of the query string and just goes back to the vanilla page
+      redirect_to back_address(""), notice: "Archived property: <b>'#{property.name}'</b>."
     else
-      redirect_to all_properties_path, notice: "Failed to archive property: <b>'#{property.name}'</b>: #{property.errors.full_messages.to_sentence}"
+      redirect_to back_address(""), notice: "Failed to archive property: <b>'#{property.name}'</b>: #{property.errors.full_messages.to_sentence}"
     end
   end
 
@@ -48,9 +50,9 @@ class PropertiesController < ApplicationController
     property.archived = false
 
     if property.save
-      redirect_to all_properties_path, notice: "Restored property to main properties list: <b>'#{property.name}'</b>."
+      redirect_to back_address(""), notice: "Restored property to main properties list: <b>'#{property.name}'</b>."
     else
-      redirect_to all_properties_path, notice: "Failed to restore property to main properties list: <b>'#{property.name}'</b>: #{property.errors.full_messages.to_sentence}"
+      redirect_to back_address(""), notice: "Failed to restore property to main properties list: <b>'#{property.name}'</b>: #{property.errors.full_messages.to_sentence}"
     end
   end
 
@@ -62,14 +64,14 @@ class PropertiesController < ApplicationController
       h.address = p[:address]
     end
     if property.save
-      redirect_to properties_path, notice: "Added property: <b>'#{property.name}'</b> to the database"
+      redirect_to back_address(""), notice: "Added property: <b>'#{property.name}'</b> to the database"
     else
       # Create 'failed_edits' hash which stores all the values from the records that failed to get saved
       failed_edits = Hash.new
       failed_edits['new'] = params[:property]
       failed_edits['new']['errors'] = property.errors.keys.map(&:to_s)
 
-      redirect_to properties_path(failed_edits), notice: "Failed to add property: <b>'#{property.name}'</b> to the database: #{property.errors.full_messages.to_sentence}"
+      redirect_to back_address(failed_edits.to_param), notice: "Failed to add property: <b>'#{property.name}'</b> to the database: #{property.errors.full_messages.to_sentence}"
     end
   end
 
@@ -96,12 +98,17 @@ class PropertiesController < ApplicationController
         end
       end
     end
-    redirect_to properties_path(failed_edits), :notice => "Updated <b>#{updated_rows}</b> row(s)" + error_str
+    redirect_to back_address(failed_edits.to_param), :notice => "Updated <b>#{updated_rows}</b> row(s)" + error_str
   end
 
   # Private method that sets Strong Parameter permissions
   private
   def allowed_params(p_id)
     params.require(:properties).require(p_id).permit(:name, :address, :archived)
+  end
+
+  # returns address that request came from, either with or without params included
+  def back_address param_string
+    !param_string.blank? ? (return "#{request.env['HTTP_REFERER']}?#{param_string}") : (return request.env['HTTP_REFERER'].split('?')[0])
   end
 end
--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -30,9 +30,9 @@ class TenantsController < ApplicationController
     tenant = Tenant.find(params[:id])
 
     if tenant.destroy
-      redirect_to tenants_path, notice: "Deleted tenant: <b>'#{tenant.name}'</b> from the database"
+      redirect_to back_address(""), notice: "Deleted tenant: <b>'#{tenant.name}'</b> from the database"
     else
-      redirect_to tenants_path, notice: "Failed to delete tenant: <b>'#{tenant.name}'</b> from the database: #{tenant.errors.full_messages.to_sentence}"
+      redirect_to back_address(""), notice: "Failed to delete tenant: <b>'#{tenant.name}'</b> from the database: #{tenant.errors.full_messages.to_sentence}"
     end
   end
 
@@ -42,9 +42,12 @@ class TenantsController < ApplicationController
     tenant.archived = true
 
     if tenant.save
-      redirect_to archived_tenants_path, notice: "Archived tenant: <b>'#{tenant.name}'</b>."
+      # Using HTTP_REFERER, because we don't know if the page we came from is properties/all or properties/archived etc
+      # split('?')[0] gets rid of the query string and just goes back to the vanilla page
+      # Using :back sends back the full query string too :(
+      redirect_to back_address(""), notice: "Archived tenant: <b>'#{tenant.name}'</b>."
     else
-      redirect_to all_tenants_path, notice: "Failed to archive tenant: <b>'#{tenant.name}'</b>: #{tenant.errors.full_messages.to_sentence}"
+      redirect_to back_address(""), notice: "Failed to archive tenant: <b>'#{tenant.name}'</b>: #{tenant.errors.full_messages.to_sentence}"
     end
   end
 
@@ -54,9 +57,9 @@ class TenantsController < ApplicationController
     tenant.archived = false
 
     if tenant.save
-      redirect_to all_tenants_path, notice: "Restored tenant to main tenants list: <b>'#{tenant.name}'</b>."
+      redirect_to back_address(""), notice: "Restored tenant to main tenants list: <b>'#{tenant.name}'</b>."
     else
-      redirect_to all_tenants_path, notice: "Failed to restore tenant to main tenants list: <b>'#{tenant.name}'</b>: #{tenant.errors.full_messages.to_sentence}"
+      redirect_to back_address(""), notice: "Failed to restore tenant to main tenants list: <b>'#{tenant.name}'</b>: #{tenant.errors.full_messages.to_sentence}"
     end
   end
 
@@ -71,14 +74,14 @@ class TenantsController < ApplicationController
       t.property_id = p[:property_id]
     end
     if tenant.save
-      redirect_to tenants_path, notice: "Added tenant <b>'#{tenant.name}'</b> to the database"
+      redirect_to back_address(""), notice: "Added tenant <b>'#{tenant.name}'</b> to the database"
     else
       # Create 'failed_edits' hash which stores all the values from the records that failed to get saved
       failed_edits = Hash.new
       failed_edits['new'] = params[:tenant]
       failed_edits['new']['errors'] = tenant.errors.keys.map(&:to_s)
 
-      redirect_to tenants_path(failed_edits), notice: "Failed to add tenant <b>'#{tenant.name}'</b> to the database: #{tenant.errors.full_messages.to_sentence}"
+      redirect_to back_address(failed_edits.to_param), notice: "Failed to add tenant <b>'#{tenant.name}'</b> to the database: #{tenant.errors.full_messages.to_sentence}"
     end
   end
 
@@ -105,12 +108,17 @@ class TenantsController < ApplicationController
         end
       end
     end
-    redirect_to tenants_path(failed_edits), :notice => "Updated <b>#{updated_rows}</b> row(s)" + error_str
+    redirect_to back_address(failed_edits.to_param), :notice => "Updated <b>#{updated_rows}</b> row(s)" + error_str
   end
 
   # Private method that sets Strong Parameter permissions
   private
   def allowed_params(t_id)
     params.require(:tenants).require(t_id).permit(:name, :payment_handle, :phone_num, :email, :property_id)
+  end
+
+  # returns address that request came from, either with or without params included
+  def back_address param_string
+    !param_string.blank? ? (return "#{request.env['HTTP_REFERER']}?#{param_string}") : (return request.env['HTTP_REFERER'].split('?')[0])
   end
 end
